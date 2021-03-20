ac1ae35f85bba8cab099dae166b8b4f772fa9a01
Alessandra Bilardi <alessandra.bilardi@gmail.com>
2021-03-21 00:36:13 +0100

    step 7 - add myType

---
+++ src/myType.ts
@@ -0,0 +1,9 @@
+/**
+ * An example of type
+ *
+ * There is only a type statement.
+ *
+ * **license**: MIT,
+ * **see**: https://github.com/bilardi/typescript-prototype
+ */
+export type Corge = boolean | number;
